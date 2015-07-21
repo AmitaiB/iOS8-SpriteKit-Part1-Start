57056952a3e7677129a4677c07cf5d930004a73c
AmitaiB <blickstein@gmail.com>
2015-07-21 17:42:11 -0400

    adds wickedCat programmatically

--- SKiOS8NewFeatTutorial/LightingScene.m
+++ SKiOS8NewFeatTutorial/LightingScene.m
@@ -11,7 +11,13 @@
 @implementation LightingScene
 
 -(void)didMoveToView:(SKView *)view {
-    
+    SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Wicked-Cat"];
+    [sprite setPosition:CGPointMake(self.frame.size.width/2, self.frame.size.height/2)];
+    [sprite setScale:0.6];
+    [sprite setZPosition:1];
+    [sprite setShadowedBitMask:1];
+    [sprite setLightingBitMask:1];
+    [self addChild:sprite];
 }
 
 @end
