dd7326277bbb145dcf0cc0b15989754d051f235e
AmitaiB <blickstein@gmail.com>
2015-07-22 13:51:38 -0400

    adds a SceneKit object into spriteKit as a 2D thingy

--- SKiOS8NewFeatTutorial/SceneKitScene.m
+++ SKiOS8NewFeatTutorial/SceneKitScene.m
@@ -7,10 +7,20 @@
 //
 
 #import "SceneKitScene.h"
+#import <SceneKit/SceneKit.h>
 
 @implementation SceneKitScene
 
 -(void)didMoveToView:(SKView *)view {
+    SCNScene *shipScene = [SCNScene sceneNamed:@"ship.dae"];
+
+    
+    SK3DNode *sk3DNodeFist = [[SK3DNode alloc]initWithViewportSize:CGSizeMake(300, 300)];
+    [sk3DNodeFist setPosition:CGPointMake(200, 200)];
+    [sk3DNodeFist setScnScene:shipScene];
+    
+    [self addChild:sk3DNodeFist];
+    [self setBackgroundColor:[SKColor greenColor]];
 }
 
 @end
