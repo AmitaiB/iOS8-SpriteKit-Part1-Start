7cda4cbcc9bbdf5f5038ae0e261b802a9390b889
AmitaiB <blickstein@gmail.com>
2015-07-22 15:09:31 -0400

    Didn't crash, but didn't exactly work...

--- SKiOS8NewFeatTutorial/SceneKitScene.m
+++ SKiOS8NewFeatTutorial/SceneKitScene.m
@@ -11,16 +11,54 @@
 
 @implementation SceneKitScene
 
+
+    //Step 2...Importing an External Model
 -(void)didMoveToView:(SKView *)view {
     SCNScene *shipScene = [SCNScene sceneNamed:@"ship.dae"];
-
     
     SK3DNode *sk3DNodeFist = [[SK3DNode alloc]initWithViewportSize:CGSizeMake(300, 300)];
     [sk3DNodeFist setPosition:CGPointMake(200, 200)];
     [sk3DNodeFist setScnScene:shipScene];
     
     [self addChild:sk3DNodeFist];
+    
     [self setBackgroundColor:[SKColor greenColor]];
+    
+    
+        //Step 3...Creating a Custom Scene
+    SK3DNode *sk3DNode = [[SK3DNode alloc] initWithViewportSize:CGSizeMake(400, 400)];
+    [sk3DNode setPosition:CGPointMake(150, 200)];
+    
+    SCNScene *sceneObject = [SCNScene scene];
+        //Needs 3 nodes: Lights, Camera, and 3D action (er, object).
+//    a. Create the object needed.
+//    b. Create a node to liason with the object.
+//    c. Add the node to the parent node -- the Scene object.
+    
+        //LIGHTS!
+    SCNLight *spotLight = [SCNLight light];
+    [spotLight setType:SCNLightTypeDirectional];
+    [spotLight setColor:[SKColor redColor]];
+    
+    SCNNode *spotLightNode = [SCNNode node];
+    [spotLightNode setLight:spotLight];
+    [spotLightNode setPosition:SCNVector3Make(0, 0, 5)];
+    
+        //CAMERA!!
+    SCNCamera *camera = [SCNCamera camera];
+    SCNNode *cameraNode = [SCNNode node];
+    [cameraNode setCamera:camera];
+    [cameraNode setPosition:SCNVector3Make(0, 0, 40)];
+    [sceneObject.rootNode addChildNode:cameraNode];
+    [cameraNode addChildNode:spotLightNode];
+    [sceneObject.rootNode addChildNode:spotLightNode];
+    
+        //SCENE (action) OBJECT!!!
+    [sk3DNode setScnScene:sceneObject];
+    [self addChild:sk3DNode];
+    
+    
+    
 }
 
 @end
